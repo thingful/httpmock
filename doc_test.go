246e9dfbb98ba88200b03cafcaf23fbc8f7572be
Sam Mulube <sam@umbrellium.co.uk>
2016-09-09 23:30:52 +0100

    Add another example to doc_test.go

--- doc_test.go
+++ doc_test.go
@@ -20,20 +20,79 @@ func ExampleRegisterStubRequest() {
 
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
-		// handle error
+		// handle error properly in real code
+		panic(err)
 	}
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		// handle error
+		// handle error properly in real code
+		panic(err)
 	}
 
 	fmt.Println(string(body))
 
 	if err = AllStubsCalled(); err != nil {
-		// handle error
+		// handle error properly in real code
+		panic(err)
 	}
 
 	// Output: ok
+}
+
+func ExampleRegisterStubRequest_WithHeader() {
+	Activate()
+	defer DeactivateAndReset()
+
+	RegisterStubRequest(
+		NewStubRequest(
+			"GET",
+			"http://example.com/",
+			NewStringResponder(200, "ok"),
+		).WithHeader(
+			&http.Header{
+				"Authorization": []string{"Bearer api-key"},
+			},
+		),
+	)
+
+	_, err := http.Get("http://example.com/")
+	if err != nil {
+		fmt.Println("Error without header")
+	}
+
+	req, err := http.NewRequest("GET", "http://example.com/", nil)
+	if err != nil {
+		// handle error properly
+		panic(err)
+	}
+
+	req.Header.Add("Authorization", "Bearer api-key")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		// handle error properly in real code
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error properly in real code
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+
+	if err = AllStubsCalled(); err != nil {
+		// handle error properly in real code
+		panic(err)
+	}
+
+	// Output:
+	// Error without header
+	// ok
 }
