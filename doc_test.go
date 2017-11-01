8e55491addb8f4b16ea9cce895a66cec5d7dee96
Sam Mulube <sam@thingful.net>
2017-11-01 19:37:41 +0000

    Update doc test to use the new APIs.

--- doc_test.go
+++ doc_test.go
@@ -8,15 +8,20 @@ import (
 	"github.com/thingful/httpmock"
 )
 
-func ExampleRegisterStubRequest() {
+func ExampleRegisterStubRequests() {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	httpmock.RegisterStubRequest(
+	httpmock.RegisterStubRequests(
 		httpmock.NewStubRequest(
 			"GET",
 			"http://example.com/",
 			httpmock.NewStringResponder(200, "ok"),
+		),
+		httpmock.NewStubRequest(
+			"GET",
+			"http://another.com/",
+			httpmock.NewStringResponder(200, "also ok"),
 		),
 	)
 
@@ -35,27 +40,45 @@ func ExampleRegisterStubRequest() {
 
 	fmt.Println(string(body))
 
+	resp, err = http.Get("http://another.com/")
+	if err != nil {
+		// handle error properly in real code
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error properly in real code
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+
 	if err = httpmock.AllStubsCalled(); err != nil {
 		// handle error properly in real code
 		panic(err)
 	}
 
-	// Output: ok
+	// Output:
+	// ok
+	// also ok
 }
 
 func ExampleRegisterStubRequest_WithHeader() {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	httpmock.RegisterStubRequest(
+	httpmock.RegisterStubRequests(
 		httpmock.NewStubRequest(
 			"GET",
 			"http://example.com/",
 			httpmock.NewStringResponder(200, "ok"),
-		).WithHeader(
-			&http.Header{
-				"Authorization": []string{"Bearer api-key"},
-			},
+			httpmock.WithHeader(
+				&http.Header{
+					"Authorization": []string{"Bearer api-key"},
+				},
+			),
 		),
 	)
 
